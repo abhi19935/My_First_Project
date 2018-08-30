e537d138d92a538749d5776bc3d620406f867255
abhi19935 <42777642+abhi19935@users.noreply.github.com>
2018-08-30 17:32:05 +0530

    comments updated

--- Hello.py
+++ Hello.py
@@ -3,5 +3,5 @@
 Created on Tue Aug 28 21:10:56 2018
 @author: Abhishek
 """
-import datetime
-print("'hello india it's" + str(datetime.datetime.now()))
+#import datetime
+#print("'hello india it's" + str(datetime.datetime.now()))
